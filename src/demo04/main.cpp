70590dea97f1df8915397e6ce9ce77f638902d33
tommy <tommywhy1989@gmail.com>
2019-11-21 22:49:12 +1300

    wocao,终于画出了个三角形。demo04....

--- src/demo04/main.cpp
+++ src/demo04/main.cpp
@@ -4,34 +4,79 @@
 #include <iostream>
 
 
-void processInput(GLFWwindow*);
+void processInput(GLFWwindow *);
 
+float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+};
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main(){\n"
+                                 "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main(){\n"
+                                   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);} ";
 using namespace std;
-int main(){
+
+int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GLFW_TRUE);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "demo 04", nullptr, nullptr);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "demo 04", nullptr, nullptr);
 
     glfwMakeContextCurrent(window); // 绑定上下文
     glewExperimental = true;
-    if(glewInit() != GLEW_OK){
+    if (glewInit() != GLEW_OK) {
         glfwTerminate();
         return -1;
     }
 
-    glViewport(0,0,800,600);
+    glViewport(0, 0, 800, 600);
 
-    while (!glfwWindowShouldClose(window)){
+    while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
-//        glClearColor(.2f,.3f,.3f,1.0f);
+        glClearColor(.2f, .5f, .5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES,0,3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        unsigned int VAO;
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+        unsigned int VBO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+        unsigned int vertexShader;
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
+        glCompileShader(vertexShader);
+
+        unsigned int fragmentShader;
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
+        glCompileShader(fragmentShader);
+
+        unsigned  int shaderProgram;
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram,vertexShader);
+        glAttachShader(shaderProgram,fragmentShader);
+        glLinkProgram(shaderProgram);
+
+        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3* sizeof(float),(void*)0);
+        glEnableVertexAttribArray(0);
+
+        glBindVertexArray(VAO);
+        glUseProgram(shaderProgram);
+        glDrawArrays(GL_TRIANGLES,0,3);
 
 
         glfwSwapBuffers(window);
@@ -41,9 +86,9 @@ int main(){
     return 0;
 }
 
-void processInput(GLFWwindow* window){
+void processInput(GLFWwindow *window) {
 
-    if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS){
-        glfwSetWindowShouldClose(window,true);
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
     }
 }
