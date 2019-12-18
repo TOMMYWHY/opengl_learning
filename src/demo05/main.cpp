4f9350ee735cde79ce262997bc2d57d244cef7a5
tommy <tommywhy1989@gmail.com>
2019-12-18 23:14:01 +1300

    demo5...

--- src/demo05/main.cpp
+++ src/demo05/main.cpp
@@ -2,6 +2,29 @@
 #include <glfw3.h>
 #include <iostream>
 
+float vertices[] = {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f
+};
+
+// 顶点着色器源码
+const char *vertex_shader_source =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        " gl_Position = vec4(aPos, 1.0);\n" "}\n\0";
+
+const char *fragment_shader_source =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" "}\n\0";
 
 int main(){
     glfwInit();
@@ -12,11 +35,7 @@ int main(){
     glfwWindowHint(GLFW_RESIZABLE, false);
 
     auto window = glfwCreateWindow(800,600,"demo05", nullptr, nullptr);
-    if (window == nullptr) {                                        // 如果窗口创建失败，输出Failed to Create OpenGL Context
-        std::cout << "Failed to Create OpenGL Context" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+
     glfwMakeContextCurrent(window);
 
 
@@ -28,9 +47,46 @@ int main(){
 
     glViewport(0,0,800,600);
 
+//    VAO VBO
+    unsigned int vertex_buffer_object; // VBO
+    glGenBuffers(1, &vertex_buffer_object);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+    GLuint vertex_array_object;  // VAO
+    glGenVertexArrays(1, &vertex_array_object);
+    glBindVertexArray(vertex_array_object);
+
+// glVertexAttribPointer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+
+    int vertexShaderId = glCreateShader(GL_VERTEX_SHADER);  // 创建 vertex shader
+    glShaderSource(vertexShaderId, 1, &vertex_shader_source, nullptr); // 声明 id 指向 shader source
+    glCompileShader(vertexShaderId);  // 编译。。。
+
+    int fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderId, 1, &fragment_shader_source, nullptr);
+    glCompileShader(fragmentShaderId);
+
+    int shaderProgramId = glCreateProgram();    // 创建 gpu 程序
+    glAttachShader(shaderProgramId, vertexShaderId);  // 添加
+    glAttachShader(shaderProgramId, fragmentShaderId);
+    glLinkProgram(shaderProgramId);     // 连接
+
+    glUseProgram(shaderProgramId);
+
+
+
     while(!glfwWindowShouldClose(window)){
         glClearColor(0.0f, 0.34f, 0.57f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgramId);
+        glBindVertexArray(vertex_array_object);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
